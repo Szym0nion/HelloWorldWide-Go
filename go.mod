d9fea2df5f9a23206de211be7d2120098f3cb163
Szymon Chmielewski <szymon.chmielewski2908@gmail.com>
2023-10-07 23:26:25 +0200

    Adding languages 1/3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Szym0nion/HelloWorldWide-Go
+
+go 1.21.2
---
+++ helloworldwide.go
@@ -0,0 +1,223 @@
+package helloworldwide
+
+import "fmt"
+
+//English
+func ENG() {
+	fmt.Println("Hello !")
+}
+
+//Afrikaans
+func AF() {
+	fmt.Println("Hallo")
+}
+
+//Aymara
+func AJ() {
+	fmt.Println("Kamisaki !")
+}
+
+//Akan
+func AK() {
+	fmt.Println("Hɛlo !")
+}
+
+//Albanian
+func SQ() {
+	fmt.Println("Përshëndetje !")
+}
+
+//Amharic
+func AM() {
+	fmt.Println("ሀሎ !")
+}
+
+//Arabic
+func AR() {
+	fmt.Println("مرحبًا !")
+}
+
+//Assamese
+func AS() {
+	fmt.Println("নমস্কাৰ !")
+}
+
+//Azerbaijani
+func AZ() {
+	fmt.Println("Salam !")
+}
+
+//Bambara
+func BM() {
+	fmt.Println("aw ni baara !")
+}
+
+//Basque
+func EU() {
+	fmt.Println("Kaixo !")
+}
+
+//Bengali
+func BN() {
+	fmt.Println("হ্যালো !")
+}
+
+//Bhojpuri
+func BH() {
+	fmt.Println("प्रणाम !")
+}
+
+//Byelorussian
+func BE() {
+	fmt.Println("добры дзень !")
+}
+
+//Burmese
+func MY() {
+	fmt.Println("မင်္ဂလာပါ !")
+}
+
+//Bosnian
+func BS() {
+	fmt.Println("zdravo !")
+}
+
+//Bulgarian
+func BG() {
+	fmt.Println("Здравейте !")
+}
+
+//Cebuano
+func CE() {
+	fmt.Println("Hello !")
+}
+
+//Chinese
+func ZH() {
+	fmt.Println("你好 !")
+}
+
+//Croatian
+func HR() {
+	fmt.Println("zdravo !")
+}
+
+//Czech
+func CZ() {
+	fmt.Println("Ahoj !")
+}
+
+//Dogri
+func DO() {
+	fmt.Println("नमस्कार !")
+}
+
+//Danish
+func DA() {
+	fmt.Println("Hej !")
+}
+
+//Esperanto
+func EO() {
+	fmt.Println("Saluton !")
+}
+
+//Estonian
+func ET() {
+	fmt.Println("Tere !")
+}
+
+//Eve
+func EE() {
+	fmt.Println("Hello !")
+}
+
+//Filipino
+func PH() {
+	fmt.Println("Kamusta !")
+}
+
+//Finnish
+func FI() {
+	fmt.Println("Hei !")
+}
+
+//French
+func FR() {
+	fmt.Println("Bonjour !")
+}
+
+//Galician
+func GL() {
+	fmt.Println("Ola !")
+}
+
+//Ganda
+func LG() {
+	fmt.Println("Nkulamusizza !")
+}
+
+//Greek
+func EL() {
+	fmt.Println("Γειά σου !")
+}
+
+//Georgian
+func KA() {
+	fmt.Println("გამარჯობა !")
+}
+
+//Guarani
+func PYG() {
+	fmt.Println("Mba'éichapa !")
+}
+
+//Gujarati
+func GU() {
+	fmt.Println("નમસ્તે !")
+}
+
+//Hausa
+func HA() {
+	fmt.Println("Sannu !")
+}
+
+//Hawaiian
+func USHI() {
+	fmt.Println("Aloha !")
+}
+
+//Hebrew
+func HE() {
+	fmt.Println("שלום !")
+}
+
+//Hindi
+func HI() {
+	fmt.Println("नमस्ते !")
+}
+
+//Spanish
+func ES() {
+	fmt.Println("Hola !")
+}
+
+//Hmong
+func HM() {
+	fmt.Println("Nyob zoo !")
+}
+
+//Igbo
+func IG() {
+	fmt.Println("Nnọọ !")
+}
+
+//Ilokano
+func IL() {
+	fmt.Println("Hello !")
+}
+
+//Indonesian
+func ID() {
+	fmt.Println("Halo !")
+}
